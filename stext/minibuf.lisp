07302616e22d07f27b0a6dc17a86ebf3739af9ad
stacksmith <stack@apple2.x10.mx>
2017-01-25 01:13:12 -0800

    OK

--- stext/minibuf.lisp
+++ stext/minibuf.lisp
@@ -9,11 +9,28 @@
  
   )
 
-(defmethod -on-key-press ((view minibuf) event from) 
+(defmethod -on-key-press ((view minibuf) event from)
+  (let ((code (gdk-event-key-keyval event) ))
+    (case code
+      (#xFF0D 
+       (let ((iter (gtb-get-iter-at-mark pbuf (gtb-get-insert pbuf))))
+	 ;; only allow input at the end!
+	 (if (gti-is-end iter)
+	     (gdk-threads-add-idle #'pbuf-idle-entry))))
+      (t (write-char (code-char code) pbuf)
+	 (finish-output pbuf))))
+
+
+  
+  
   (let ((stream (gtk-text-view-buffer view)))
     (format stream "~C" (code-char (gdk-event-key-keyval event)))
     (finish-output stream)
     (-on-key-press from event nil)))
+
+
+
+
 
 
 
